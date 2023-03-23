983ca8a22609f40739d112e4bcc0772c3bfd98eb
zhang722 <1755169086@qq.com>
2023-03-23 10:38:46 +0800

    lsr done

--- lsr/src/lib.rs
+++ lsr/src/lib.rs
@@ -1,6 +1,11 @@
 use std::error::Error;
+use std::fs;
+use std::time::SystemTime;
 
+use chrono::Datelike;
 use clap::{App, Arg};
+use tabular::{Table, Row};
+use chrono::{NaiveDateTime};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -37,6 +42,47 @@ pub fn get_flags() -> MyResult<Config> {
     Ok(Config { entries, show_hidden, long})
 }
 
-pub fn run() -> MyResult<()> {
+pub fn run(config: &Config) -> MyResult<()> {
+    for entry in &config.entries {
+        let entries = fs::read_dir(entry)?;
+        let mut table = Table::new("{:<}  {:<}  {:<}");
+        for entry in entries {
+            let path = entry?.path();
+            if let Some(path) = path.to_str() {
+                let mut show: bool = false;
+
+#[cfg(target_os="windows")]
+                if config.show_hidden || !path.starts_with(r".\."){
+                    show = true;
+                } 
+
+#[cfg(target_os="linux")] 
+                if config.show_hidden || !path.starts_with(r"."){
+                    show = true;
+                }
+
+                if show {
+                    let modified = fs::metadata(path)?.modified()?;
+                    let dt: NaiveDateTime = NaiveDateTime::from_timestamp_opt(
+                        modified.duration_since(SystemTime::UNIX_EPOCH)?.as_secs() as i64,
+                        0,
+                    ).unwrap();
+                    
+                    if config.long {
+                        table.add_row(Row::new()
+                            .with_cell(format!("{}/{}/{}", dt.year(), dt.month(), dt.day()))
+                            .with_cell(dt.time())
+                            .with_cell(path));
+                    } else {
+                        println!("{}", path);
+                    }
+                }
+
+            }
+            
+        }
+        println!("{}", table);
+    }
+    
     Ok(())
 }
--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     if let Ok(config) = lsr::get_flags() {
         println!("{:?}", config);
+        lsr::run(&config).unwrap();
     }
 }
